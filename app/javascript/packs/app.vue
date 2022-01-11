eda22ac656e32178f5ce753e32a5519dbea79c5e
ekzema <xxx.zp@mail.ru>
2022-01-11 15:58:06 +0200

    Added bootstrap to Vue.js

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -1,13 +1,178 @@
 <template>
-    <p>{{ message }}</p>
+  <div class="wrapper home-default-wrapper">
+    <!--    Start Header Wrapper-->
+    <header class="header-wrapper">
+      <div class="header-top">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-sm-5 col-md-4 col-lg-6">
+              <div class="header-info-left">
+                <p>Free Returns and Free Shipping</p>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-7 col-md-8 col-lg-6 sm-pl-0 xs-pl-15 header-top-right">
+              <div class="header-info">
+                <a href="tel://+00123456789"><i class="fa fa-phone"></i> +00 123 456 789</a>
+                <a href="mailto://demo@example.com"><i class="fa fa-envelope"></i> demo@example.com</a>
+                <a href="login-register.html"><i class="fa fa-user"></i> Account</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="header-middle">
+        <div class="container">
+          <div class="row row-gutter-0 align-items-center">
+            <div class="col-12">
+              <div class="header-align">
+                <div class="header-align-left">
+                  <div class="header-logo-area">
+                    <a href="index.html">
+                      <img class="logo-main" src="./assets/img/logo.png" alt="Logo" />
+                      <img class="logo-light" src="./assets/img/logo.png" alt="Logo" />
+                    </a>
+                  </div>
+                </div>
+                <div class="header-align-center">
+                  <div class="header-search-box">
+                    <form action="#" method="post">
+                      <div class="form-input-item">
+                        <label for="search" class="sr-only">Search Everything</label>
+                        <input type="text" id="search" placeholder="Search Everything">
+                        <button type="submit" class="btn-src">
+                          <i class="pe-7s-search"></i>
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+                <div class="header-align-right">
+                  <div class="header-action-area">
+                    <div class="header-action-wishlist">
+                      <button class="btn-wishlist" onclick="window.location.href='shop-wishlist.html'">
+                        <i class="pe-7s-like"></i>
+                      </button>
+                    </div>
+                    <div class="header-action-cart">
+                      <button class="btn-cart cart-icon">
+                        <span class="cart-count">01</span>
+                        <i class="pe-7s-shopbag"></i>
+                      </button>
+                    </div>
+                    <button class="btn-menu d-md-none">
+                      <i class="ion-navicon"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="header-area header-default sticky-header">
+        <div class="container">
+          <div class="row row-gutter-0 align-items-center">
+            <div class="col-4 col-sm-6 col-lg-2">
+              <div class="header-logo-area">
+                <a href="index.html">
+                  <img class="logo-main" src="assets/img/logo.png" alt="Logo" />
+                  <img class="logo-light" src="assets/img/logo.png" alt="Logo" />
+                </a>
+              </div>
+            </div>
+            <div class="col-lg-8 sticky-md-none">
+              <div class="header-navigation-area d-none d-md-block">
+                <ul class="main-menu nav position-relative">
+                  <li class="has-submenu active"><a class="ml--2" href="index.html">Home</a>
+                    <ul class="submenu-nav">
+                      <li><a href="index.html">Home One</a></li>
+                      <li><a href="index-2.html">Home Two</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="about.html">About</a></li>
+                  <li class="has-submenu"><a href="index.html">Pages</a>
+                    <ul class="submenu-nav">
+                      <li><a href="shop-cart.html">Cart page</a></li>
+                      <li><a href="shop-checkout.html">Checkout</a></li>
+                      <li><a href="my-account.html">My Account</a></li>
+                      <li><a href="shop-wishlist.html">Wishlist</a></li>
+                      <li><a href="shop-compare.html">Compare</a></li>
+                      <li><a href="login-register.html">Login / Register</a></li>
+                      <li><a href="coming-soon.html">Coming soon</a></li>
+                      <li><a href="page-not-found.html">404</a></li>
+                    </ul>
+                  </li>
+                  <li class="has-submenu"><a href="shop.html">Shop</a>
+                    <ul class="submenu-nav">
+                      <li><a href="shop-3-column.html">Shop 3 Column</a></li>
+                      <li><a href="shop.html">Shop 4 Column</a></li>
+                      <li><a href="shop-left-sidebar.html">Shop Left Sidebar</a></li>
+                      <li><a href="shop-right-sidebar.html">Shop Right Sidebar</a></li>
+                      <li><a href="shop.html">Shop No Sidebar</a></li>
+                      <li><a href="shop-single-product.html">Product Details</a></li>
+                      <li><a href="shop-single-product-variable.html">Product Variable</a></li>
+                      <li><a href="shop-single-product-grouped.html">Product Grouped</a></li>
+                    </ul>
+                  </li>
+                  <li class="has-submenu"><a href="blog.html">Blog</a>
+                    <ul class="submenu-nav">
+                      <li><a href="blog.html">Blog 3 Column</a></li>
+                      <li><a href="blog-4-column.html">Blog 4 Column</a></li>
+                      <li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
+                      <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
+                      <li><a href="blog.html">Blog No Sidebar</a></li>
+                      <li><a href="blog-details.html">Blog Details Left Sidebar</a></li>
+                      <li><a href="blog-details-right-sidebar.html">Blog Details Right Sidebar</a></li>
+                      <li><a href="blog-details-no-sidebar.html">Blog Details No Sidebar</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="contact.html">Contact</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-8 col-sm-6 col-lg-2">
+              <div class="header-action-area">
+                <div class="header-action-search">
+                  <button class="btn-search btn-search-menu">
+                    <i class="pe-7s-search"></i>
+                  </button>
+                </div>
+                <div class="header-action-login">
+                  <button class="btn-login" onclick="window.location.href='login-register.html'">
+                    <i class="pe-7s-users"></i>
+                  </button>
+                </div>
+                <div class="header-action-wishlist">
+                  <button class="btn-wishlist" onclick="window.location.href='shop-wishlist.html'">
+                    <i class="pe-7s-like"></i>
+                  </button>
+                </div>
+                <div class="header-action-cart">
+                  <button class="btn-cart cart-icon">
+                    <span class="cart-count">01</span>
+                    <i class="pe-7s-shopbag"></i>
+                  </button>
+                </div>
+                <button class="btn-menu d-lg-none">
+                  <i class="ion-navicon"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!--    End Header Wrapper-->
+  </div>
 </template>
 
 <script>
+import './assets/css/style.css'
 
 export default {
   data: function () {
     return {
-      message: "Main page"
+      message: "YOUR CODES GOES HERE"
     }
   },
   created () {
@@ -16,8 +181,4 @@ export default {
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
 </style>
--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './app'
 import router from './router/shop.js'
 import store from './store/shop'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 document.addEventListener('DOMContentLoaded', () => {
   createApp(App)
