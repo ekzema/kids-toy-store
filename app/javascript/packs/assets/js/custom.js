064858e3c1fa815510e6faff4f20a12fdf831cd6
Denis Kuzmenko <https://bitbucket.org/>
2022-01-16 09:31:20 +0200

    Added libs js

---
+++ app/javascript/packs/assets/js/custom.js
@@ -0,0 +1,12 @@
+import $ from 'jquery'
+import AOS from 'aos'
+// import 'slicknav'
+
+    let dsdsd = $('.main-menu').slicknav({
+        appendTo: '.res-mobile-menu',
+        closeOnClick: true,
+        removeClasses: true,
+        closedSymbol: '<i class="fa fa-angle-down"></i>',
+        openedSymbol: '<i class="fa fa-angle-up"></i>'
+    });
+    console.log(dsdsd,'test')
--- app/javascript/packs/views/home/Index.vue
+++ app/javascript/packs/views/home/Index.vue
@@ -1,10 +1,10 @@
 <template>
   <main-slider/>
+  {{}}
 </template>
 
 <script>
 import MainSlider from './components/Slider'
-
 export default {
   components: {
     MainSlider
@@ -14,6 +14,9 @@ export default {
     }
   },
   methods: {
+  },
+  created() {
+
   }
 }
 </script>
