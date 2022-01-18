3484ff97f87551510f0048e81e1a4ef5de72b33e
ekzema <xxx.zp@mail.ru>
2022-01-18 10:15:04 +0200

    Added isotope

--- app/javascript/packs/assets/js/custom.js
+++ app/javascript/packs/assets/js/custom.js
@@ -2,6 +2,10 @@ import AOS from 'aos'
 import 'slicknav/dist/jquery.slicknav.min'
 import Swiper from 'swiper'
 import 'jquery.countdown/jquery.countdown' // use method countDown
+import jQueryBridget from 'jquery-bridget'
+import Isotope from 'isotope-layout'
+
+jQueryBridget('isotope', Isotope, $)
 
 
 
