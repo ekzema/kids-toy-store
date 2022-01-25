6a2b18de2052a0af33ee88e40527baa6102dd31c
Denis Kuzmenko <https://bitbucket.org/>
2022-01-25 17:24:31 +0200

    Added burger button

--- app/javascript/packs/components/app/Header.vue
+++ app/javascript/packs/components/app/Header.vue
@@ -70,61 +70,19 @@
     <div class="header-area header-default sticky-header">
       <div class="container">
         <div class="row row-gutter-0 align-items-center">
-          <div class="col-4 col-sm-6 col-lg-2">
-            <div class="header-logo-area">
-              <a href="index.html">
-                <img class="logo-main" src="./../../assets/img/logo.png" alt="Logo" />
-                <img class="logo-light" src="./../../assets/img/logo.png" alt="Logo" />
-              </a>
-            </div>
-          </div>
           <div class="col-lg-8 sticky-md-none">
             <div class="header-navigation-area d-none d-md-block">
               <ul class="main-menu nav position-relative">
-                <li class="has-submenu active"><a class="ml--2" href="index.html">Home</a>
-                  <ul class="submenu-nav">
-                    <li><a href="index.html">Home One</a></li>
-                    <li><a href="index-2.html">Home Two</a></li>
-                  </ul>
-                </li>
-                <li><a href="about.html">About</a></li>
-                <li class="has-submenu"><a href="index.html">Pages</a>
-                  <ul class="submenu-nav">
-                    <li><a href="shop-cart.html">Cart page</a></li>
-                    <li><a href="shop-checkout.html">Checkout</a></li>
-                    <li><a href="my-account.html">My Account</a></li>
-                    <li><a href="shop-wishlist.html">Wishlist</a></li>
-                    <li><a href="shop-compare.html">Compare</a></li>
-                    <li><a href="login-register.html">Login / Register</a></li>
-                    <li><a href="coming-soon.html">Coming soon</a></li>
-                    <li><a href="page-not-found.html">404</a></li>
-                  </ul>
-                </li>
-                <li class="has-submenu"><a href="shop.html">Shop</a>
-                  <ul class="submenu-nav">
-                    <li><a href="shop-3-column.html">Shop 3 Column</a></li>
-                    <li><a href="shop.html">Shop 4 Column</a></li>
-                    <li><a href="shop-left-sidebar.html">Shop Left Sidebar</a></li>
-                    <li><a href="shop-right-sidebar.html">Shop Right Sidebar</a></li>
-                    <li><a href="shop.html">Shop No Sidebar</a></li>
-                    <li><a href="shop-single-product.html">Product Details</a></li>
-                    <li><a href="shop-single-product-variable.html">Product Variable</a></li>
-                    <li><a href="shop-single-product-grouped.html">Product Grouped</a></li>
-                  </ul>
-                </li>
-                <li class="has-submenu"><a href="blog.html">Blog</a>
-                  <ul class="submenu-nav">
-                    <li><a href="blog.html">Blog 3 Column</a></li>
-                    <li><a href="blog-4-column.html">Blog 4 Column</a></li>
-                    <li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
-                    <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
-                    <li><a href="blog.html">Blog No Sidebar</a></li>
-                    <li><a href="blog-details.html">Blog Details Left Sidebar</a></li>
-                    <li><a href="blog-details-right-sidebar.html">Blog Details Right Sidebar</a></li>
-                    <li><a href="blog-details-no-sidebar.html">Blog Details No Sidebar</a></li>
-                  </ul>
+                <li>
+                  <div class="menu">
+                    <button class="menu__btn js-menu-btn">
+                      <span class="menu__burger"><span></span></span><span class="menu__text">Каталог товаров</span>
+                    </button>
+                  </div>
                 </li>
                 <li><a href="contact.html">Contact</a></li>
+                <li><a href="contact.html">About</a></li>
+                <li><a href="contact.html">Blogs</a></li>
               </ul>
             </div>
           </div>
