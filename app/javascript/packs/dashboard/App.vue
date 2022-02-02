76728f06c74f954942a42d069bb59263499eef58
ekzema <xxx.zp@mail.ru>
2022-02-02 18:13:33 +0200

    Change structure application

---
+++ app/javascript/packs/dashboard/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Dashboard</h1>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data () {
+    {
+    }
+  },
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ app/javascript/packs/dashboard/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App'
+
+document.addEventListener('DOMContentLoaded', () => {
+    createApp(App)
+        .mount('#app')
+})
