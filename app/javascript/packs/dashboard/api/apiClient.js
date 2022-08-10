044f64ced5bbf608d2439d2fd9c17a34270781a1
ekzema <xxx.zp@mail.ru>
2022-08-10 16:27:16 +0300

    Added Api class

---
+++ app/javascript/packs/dashboard/api/apiClient.js
@@ -0,0 +1,44 @@
+class ApiClient {
+    constructor(resource, options = {}) {
+        this.apiVersion = `/api/${options.apiVersion}`
+        this.options = options
+        this.resource = resource
+    }
+
+    get url() {
+        let url = this.apiVersion;
+        if (this.options.accountScoped) {
+            const isInsideAccountScopedURLs = window.location.pathname.includes(
+                '/app/accounts'
+            );
+
+            if (isInsideAccountScopedURLs) {
+                const accountId = window.location.pathname.split('/')[3];
+                url = `${url}/accounts/${accountId}`;
+            }
+        }
+        return `${url}/${this.resource}`;
+    }
+
+    get() {
+        return axios.get(this.url);
+    }
+
+    show(id) {
+        return axios.get(`${this.url}/${id}`);
+    }
+
+    create(data) {
+        return axios.post(this.url, data);
+    }
+
+    update(id, data) {
+        return axios.patch(`${this.url}/${id}`, data);
+    }
+
+    delete(id) {
+        return axios.delete(`${this.url}/${id}`);
+    }
+}
+
+export default ApiClient;
