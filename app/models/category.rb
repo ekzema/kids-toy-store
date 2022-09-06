774f1205ebbb480517ba4da794a68b3557687b19
ekzema <xxx.zp@mail.ru>
2022-09-06 16:41:14 +0300

    Clean dependence children after delete category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,11 @@
 class Category < ApplicationRecord
   belongs_to :parent, :class_name => "Category", optional: true
-
   validates :name, presence: true
+  after_destroy :clean_dependence_child
+
+  private
+
+  def clean_dependence_child
+    Category.where(parent_id: id).update_all(parent_id: nil)
+  end
 end
