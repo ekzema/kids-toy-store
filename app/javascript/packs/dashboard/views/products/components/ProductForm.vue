de3fb0884485222cda95741259c56c062ad00a74
denis <ekzemadev@gmail.com>
2022-12-15 17:18:06 +0200

    Fixed lang category in product

--- app/javascript/packs/dashboard/views/products/components/ProductForm.vue
+++ app/javascript/packs/dashboard/views/products/components/ProductForm.vue
@@ -92,20 +92,20 @@
         <v-chip
             class="ma-1"
             v-bind="props"
-            :text="item.raw.name"
+            :text="item.raw.name[language]"
             color="primary"
             closable
             size="default"
             variant="outlined"
         >
-          <strong>{{ item.raw.name }}</strong>&nbsp;
-          <span style="font-size: 11px">{{ item.raw.parent_name || 'parent' }}</span>
+          <strong>{{ item.raw.name[language] }}</strong>&nbsp;
+          <span style="font-size: 11px">{{  item.raw.parent_name ? item.raw.parent_name[language] : 'parent' }}</span>
         </v-chip>
       </template>
       <template v-slot:item="{ props, item }">
         <v-list-item
             v-bind="props"
-            :title="item.raw.name"
+            :title="item.raw.name[language]"
             :subtitle="item.raw.group"
             color="primary"
         ></v-list-item>
