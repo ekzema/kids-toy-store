97f5361adfb03079935a4b9b4eaaae2c866b5326
denis <ekzemadev@gmail.com>
2023-01-09 00:00:00 +0200

    Added component SelectCategories

---
+++ app/javascript/packs/dashboard/views/products/components/SelectCategories.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-autocomplete
+      label="Select categories"
+      v-model="form.data.categories"
+      :rules="requiredArrayRules"
+      :items="detailConstructor.categories"
+      variant="underlined"
+      item-title="name"
+      item-value="id"
+      color="primary"
+      multiple
+  >
+    <template v-slot:chip="{ props, item }">
+      <v-chip
+          class="ma-1"
+          v-bind="props"
+          color="primary"
+          closable
+          size="default"
+          variant="outlined"
+      >
+        <strong>{{ item.raw.name ? item.raw.name[language] : '' }}</strong>&nbsp;
+        <span style="font-size: 11px">{{ item.raw.parent_name ? item.raw.parent_name[language] : 'parent' }}</span>
+      </v-chip>
+    </template>
+    <template v-slot:item="{ props, item }">
+      <v-list-item
+          v-bind="props"
+          :title="item.raw.name[language]"
+          :subtitle="item.raw.group"
+          color="primary"
+      ></v-list-item>
+    </template>
+  </v-autocomplete>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'select-categories',
+  data: () => ({
+  }),
+  props: ['language'],
+  created () {
+  },
+  methods: {
+
+  },
+  computed: {
+    ...mapGetters([
+      'detailConstructor'
+    ])
+  },
+}
+</script>
