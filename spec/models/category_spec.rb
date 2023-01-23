05936f56bdf2d7bd7ae704a6850c99886c4f960b
ekzema <ekzemadev@gmail.com>
2023-01-23 23:41:25 +0200

    Added context for Category model specs

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,17 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe Category do
-  let!(:category) { create(:category) }
+  let(:category) { create(:category) }
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name).ignoring_case_sensitivity }
 
-    it 'validate parent before update' do
-      new_category = described_class.create(name: Faker::Superhero.name, parent_id: category.id)
-      category.update(parent_id: new_category.id)
-      expect(category.valid?).to be(false)
-      expect(category.errors.messages[:category]).to include('This category is already a parent!')
+    context 'with validate parent before update' do
+      let(:new_category) { create(:category, parent_id: category.id) }
+
+      before do
+        category.update(parent_id: new_category.id)
+      end
+
+      it { expect(category.valid?).to be(false) }
+      it { expect(category.errors.messages[:category]).to include('This category is already a parent!') }
     end
   end
 end
