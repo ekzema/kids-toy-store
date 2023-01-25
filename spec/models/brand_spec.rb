0e093c44a95a861b28612e2e20196c780fb54e46
ekzema <ekzemadev@gmail.com>
2023-01-25 14:07:28 +0200

    Added validations to brand_spec

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Brand do
+  describe 'associations' do
+    it { should have_many(:products).dependent(:restrict_with_exception) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:name) }
+  end
+
+  describe 'downcase name' do
+    let(:name) { Faker::Commerce.brand }
+    let(:brand) { build(:brand, name: name) }
+
+    before { brand.save }
+
+    it { expect(brand.name).not_to eq(name) }
   end
 end
