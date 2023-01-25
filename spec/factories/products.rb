c8722e8c804f21864cbdbe660bc657c5fc310997
ekzema <ekzemadev@gmail.com>
2023-01-25 13:01:56 +0200

    Added describe soft delete to product_spec

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :product do
-    
+    name { Faker::Commerce.product_name }
+    description { Faker::Lorem.paragraph }
+    code { Faker::Number.number(digits: 4) }
+    price { Faker::Number.number(digits: 3) }
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,15 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe Product do
+  describe 'associations' do
+    it { should have_many(:product_images).dependent(:destroy) }
+  end
+
   describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:code) }
-    it { should validate_presence_of(:description) }
+    %i[name code description].each do |field|
+      it { should validate_presence_of(field) }
+    end
+
     it { should validate_numericality_of(:code).only_integer.is_greater_than_or_equal_to(1000) }
     it { should validate_numericality_of(:price).only_integer.is_greater_than_or_equal_to(100) }
   end
 
-  describe 'associations' do
-    it { should have_many(:product_images).dependent(:destroy) }
+  describe 'enums' do
+    %i[status production_country for_gender].each do |field|
+      it { should define_enum_for(field) }
+    end
+  end
+
+  describe 'soft delete' do
+    let(:product) { create(:product) }
+
+    before { product.soft_delete! }
+
+    it { expect(product.deleted_at).to be_an_instance_of(ActiveSupport::TimeWithZone) }
   end
 end
