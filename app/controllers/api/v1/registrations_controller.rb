4066e6a665f78e7e0957fa580fb93cce7ed5a65c
ekzema <ekzemadev@gmail.com>
2023-01-31 12:51:01 +0200

    Changed registrations_controller

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,24 +3,18 @@
 class Api::V1::RegistrationsController < ApiController
   def create
     user = User.new(registration_params)
-    if user.save
-      payload = { user_id: user.id }
-      session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
-      tokens = session.login
 
-      response.set_cookie(JWTSessions.access_cookie,
-                          value: tokens[:access],
-                          httponly: true,
-                          secure: Rails.env.production?)
-      render json: { csrf: tokens[:csrf] }
+    if user.save
+      user.send_confirmation_email!
+      render_response(expand: { message: 'Please check your email for confirmation instructions.' }, status: :created)
     else
-      render json: { error: user.errors.full_messages.join(' ') }, status: :unprocessable_entity
+      render json: product.errors, status: :unprocessable_entity
     end
   end
 
   private
 
   def registration_params
-    params.permit(:email, :password, :password_confirmation)
+    params.require(:registration).permit(:email, :password, :password_confirmation)
   end
 end
