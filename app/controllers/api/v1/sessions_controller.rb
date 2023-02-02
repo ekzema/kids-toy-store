0917bea64e6ec378786a85656565c2c007c8bd70
ekzema <ekzemadev@gmail.com>
2023-02-02 09:27:17 +0200

    Changed session controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,31 +1,28 @@
 # frozen_string_literal: true
 
 class Api::V1::SessionsController < ApiController
-  before_action :authorize_access_request!, only: [:destroy]
+  before_action :authorize_by_access_header!, only: [:destroy]
 
   def create
-    user = User.find_by!(email: session_params[:email])
-    return render_error_response('Cannot find user', :not_found) unless user.authenticate(session_params[:password])
+    user = User.find_by(email: session_params[:email])
+    return render_error_response('Cannot find user') unless user&.authenticate(session_params[:password])
 
     payload = { user_id: user.id }
-    session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
+    session = JWTSessions::Session.new(payload: payload)
     tokens = session.login
-    response.set_cookie(JWTSessions.access_cookie,
-                        value: tokens[:access],
-                        httponly: true,
-                        secure: Rails.env.production?)
-    render json: { csrf: tokens[:csrf] }
+
+    render_response(tokens)
   end
 
   def destroy
-    session = JWTSessions::Session.new(payload: payload)
-    session.flush_by_access_payload
-    render json: :ok
+    # session = JWTSessions::Session.new(payload: payload)
+    # session.flush_by_access_payload
+    render_response(status: :no_content)
   end
 
   private
 
   def session_params
-    params.permit(:email, :password)
+    params.require(:session).permit(:email, :password)
   end
 end
