1b69101820181f2e7d2606c294c265b4776c457e
ekzema <ekzemadev@gmail.com>
2023-02-11 14:29:55 +0200

    Made JWTSessions redis allow only dev and prod env

--- config/initializers/jwt_sessions.rb
+++ config/initializers/jwt_sessions.rb
@@ -1,9 +1,12 @@
 JWTSessions.encryption_key = ENV['SECRET_KEY_BASE']
 JWTSessions.access_exp_time = 3024000 #5 weeks
-JWTSessions.token_store = :redis, {
-  redis_host: "redis",
-  redis_port: "6379",
-  redis_db_name: "0",
-  token_prefix: "jwt_",
-  pool_size: Integer(ENV.fetch("RAILS_MAX_THREADS", 5))
-}
+
+unless Rails.env.test?
+  JWTSessions.token_store = :redis, {
+    redis_host: "redis",
+    redis_port: "6379",
+    redis_db_name: "0",
+    token_prefix: "jwt_",
+    pool_size: Integer(ENV.fetch("RAILS_MAX_THREADS", 5))
+  }
+end
