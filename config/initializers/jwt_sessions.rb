22e8b028c2f1b7bae65b232e02396d3e1f9005e4
ekzema <ekzemadev@gmail.com>
2023-02-12 21:28:28 +0200

    Fixed JWTSessions env

--- config/initializers/jwt_sessions.rb
+++ config/initializers/jwt_sessions.rb
@@ -5,7 +5,7 @@ unless Rails.env.test?
   JWTSessions.token_store = :redis, {
     redis_host: "redis",
     redis_port: "6379",
-    redis_db_name: "0",
+    redis_db_name: Rails.env.production? ? "1" : "0",
     token_prefix: "jwt_",
     pool_size: Integer(ENV.fetch("RAILS_MAX_THREADS", 5))
   }
