e03607c281dddcfedf4655b5c39b308c5e4b4a9e
ekzema <ekzemadev@gmail.com>
2023-02-17 22:29:34 +0200

    Added account login

--- app/javascript/packs/shop/router/index.js
+++ app/javascript/packs/shop/router/index.js
@@ -1,10 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeIndex from '../views/home/Index'
+import AccountNew from '../views/account/New.vue'
+import AccountLogin from '../views/account/Login.vue'
+import PageNotFound from "../../errors/PageNotFound.vue"
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: HomeIndex }
+    {
+      name: 'HomeIndex',
+      path: '/',
+      component: HomeIndex
+    },
+    {
+      name: 'AccountNew',
+      path: '/account/new',
+      component: AccountNew
+    },
+    {
+      name: 'AccountLogin',
+      path: '/account/login',
+      component: AccountLogin
+    },
+    {
+      name: 'PageNotFound',
+      path: '/:pathMatch(.*)*',
+      component: PageNotFound
+    }
   ]
 })
 
---
+++ app/javascript/packs/shop/views/account/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="login-register-area">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <div class="login-register-content">
+            <div class="login-register-title mb-30">
+              <h2 class="text-center">Login</h2>
+              <p class="text-center">Welcome back! Please enter your username and password to login. </p>
+            </div>
+            <div class="login-register-style">
+              <form action="#" method="post">
+                <div class="login-register-input">
+                  <input type="text" name="user-name" placeholder="Username or email address">
+                </div>
+                <div class="login-register-input">
+                  <input type="password" name="user-password" placeholder="Password">
+                  <div class="forgot">
+                    <a href="#">Forgot?</a>
+                  </div>
+                </div>
+                <div class="btn-style-3">
+                  <button class="btn" onclick="window.location.href='my-account.html'" type="button">Login</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  components: {
+  },
+  data: () => ({
+  }),
+  methods: {
+
+  }
+}
+</script>
