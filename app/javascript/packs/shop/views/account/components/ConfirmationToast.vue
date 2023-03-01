6fcc184569c01e492e3a0aee2ecd62d761baa7cf
ekzema <ekzemadev@gmail.com>
2023-03-01 18:05:51 +0200

    Added ConfirmationToast component

---
+++ app/javascript/packs/shop/views/account/components/ConfirmationToast.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <h3>Congratulations!</h3>
+    <span> A link to confirm your registration has been sent to your email: <strong>{{email}}</strong></span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'confirmation-toast',
+
+  data: () => ({
+
+  }),
+  props: ['email'],
+  computed: {
+
+  },
+  created() {
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  color: white;
+}
+
+span {
+  font-size: 20px;
+}
+
+
+</style>
