3e4393ac1af4c800a301b5a78f56c0d34ed17083
ekzema <ekzemadev@gmail.com>
2023-03-03 00:46:59 +0200

    Added UpdatePasswordToast component

---
+++ app/javascript/packs/shop/views/account/components/UpdatePasswordToast.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h3>Success!</h3>
+    <span>Your password has been successfully reset.</span><br>
+    <span>Now you can try logging into your account again.</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'update-password-toast',
+}
+</script>
+
+<style scoped>
+h3 {
+  color: white;
+  font-size: 18px;
+}
+
+span {
+  font-size: 18px;
+}
+</style>
