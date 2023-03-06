7ea0e043bffa1b3090deb0268bb0a71b84aef26d
ekzema <ekzemadev@gmail.com>
2023-03-06 15:35:17 +0200

    Added authorize_admin!

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class AdminController < ApiController
+  before_action :authorize_admin!
   before_action do
     self.namespace_for_serializer = Admin
   end
@@ -9,5 +10,10 @@ class AdminController < ApiController
 
   def prepare_serializer(data, class_serializer = nil, options = {})
     super { 'Admin' }
+  end
+
+  def authorize_admin!
+    authorize_by_access_header!
+    render_error_response('Access denied', :forbidden) unless current_user&.admin
   end
 end
