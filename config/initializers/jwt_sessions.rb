d7d0388a73350c3ddb13bf05f85d4ed15cfd95e0
ekzema <ekzemadev@gmail.com>
2023-03-09 22:15:20 +0200

    Changed jwt_sessions settings

--- config/initializers/jwt_sessions.rb
+++ config/initializers/jwt_sessions.rb
@@ -2,11 +2,13 @@ JWTSessions.encryption_key = ENV['SECRET_KEY_BASE']
 JWTSessions.access_exp_time = 3024000 #5 weeks
 
 unless Rails.env.test?
-  JWTSessions.token_store = :redis, {
-    redis_host: "redis",
+  options = {
     redis_port: "6379",
     redis_db_name: Rails.env.production? ? "1" : "0",
     token_prefix: "jwt_",
     pool_size: Integer(ENV.fetch("RAILS_MAX_THREADS", 5))
   }
+  options.merge!({ redis_host: "redis" }) if Rails.env.development?
+
+  JWTSessions.token_store = :redis, options
 end
