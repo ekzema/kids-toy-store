91ddcf6d570e4a89b32a917426b7cee97789ee73
ekzema <ekzemadev@gmail.com>
2023-03-19 18:27:18 +0200

    Added header_old

---
+++ app/javascript/frontend/shop/components/app/Header_old.vue
@@ -0,0 +1,224 @@
+<template>
+  <header class="header-wrapper">
+    <div class="header-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-sm-5 col-md-4 col-lg-4">
+            <div class="header-info-left">
+              <p>Free Returns and Free Shipping</p>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-7 col-md-8 col-lg-8 sm-pl-0 xs-pl-15 header-top-right">
+            <div class="header-info">
+              <a href="tel://+00123456789"><i class="fa fa-phone"></i> +00 123 456 789</a>
+              <a href="mailto://demo@example.com"><i class="fa fa-envelope"></i> demo@example.com</a>
+              <a v-if="user" href="login-register.html"><i class="fa fa-user"></i> Account</a>
+              <div class="wrap-account" v-if="!user">
+                <router-link :to="{ name: 'AccountNew'}">
+                  <i class="fa fa-user-plus"></i> Sign up
+                </router-link>
+                <router-link :to="{ name: 'AccountLogin'}">
+                  <i class="fa fa-sign-in"></i> Login
+                </router-link>
+              </div>
+              <router-link to="" @click="logout" v-else><i class="fa fa-sign-out"></i> Logout</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="header-middle">
+      <div class="container">
+        <div class="row row-gutter-0 align-items-center">
+          <div class="col-12">
+            <div class="header-align">
+              <div class="header-align-left">
+                <div class="header-logo-area">
+                  <a href="index.html">
+                    <img class="logo-main" src="./../../assets/img/logo.png" alt="Logo" />
+                    <img class="logo-light" src="./../../assets/img/logo.png" alt="Logo" />
+                  </a>
+                </div>
+              </div>
+              <div class="header-align-center">
+                <div class="header-search-box">
+                  <form action="#" method="post">
+                    <div class="form-input-item">
+                      <label for="search" class="sr-only">Search Everything</label>
+                      <input type="text" id="search" placeholder="Search Everything">
+                      <button type="submit" class="btn-src">
+                        <i class="pe-7s-search"></i>
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div class="header-align-right">
+                <div class="header-action-area">
+                  <div class="header-action-wishlist">
+                    <button class="btn-wishlist" onclick="window.location.href='shop-wishlist.html'">
+                      <i class="pe-7s-like"></i>
+                    </button>
+                  </div>
+                  <div class="header-action-cart">
+                    <button class="btn-cart cart-icon">
+                      <span class="cart-count">01</span>
+                      <i class="pe-7s-shopbag"></i>
+                    </button>
+                  </div>
+                  <button class="btn-menu d-md-none">
+                    <i class="ion-navicon"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="header-area header-default sticky-header">
+      <div class="container">
+        <div class="row row-gutter-0 align-items-center">
+          <div class="col-4 col-sm-6 col-lg-4">
+            <div class="menu">
+              <button class="menu__btn">
+                <span class="menu__burger"><span></span></span><span class="menu__text">Каталог товаров</span>
+              </button>
+              <div class="menu__wrapper">
+                <div class="menu__item" data-id="93" data-childs="31"><a class="menu__head" href="//mamazin.com.ua/igrushki-razvivajushhie/">Игрушки РАЗВИВАЮЩИЕ</a></div>
+                <div class="menu__item" data-id="260" data-childs="42"><a class="menu__head" href="//mamazin.com.ua/tvorchestvo-obuchenie/">ТВОРЧЕСТВО, поделки</a></div>
+                <div class="menu__item" data-id="1971" data-childs="10"><a class="menu__head" href="//mamazin.com.ua/knigi-magazin/">КНИГИ</a></div>
+                <div class="menu__item" data-id="721" data-childs="14"><a class="menu__head" href="//mamazin.com.ua/obuchenie-opyty/">Наборы для обучения и опытов</a></div>
+                <div class="menu__item" data-id="117" data-childs="20"><a class="menu__head" href="//mamazin.com.ua/igrushki-dlja-devochek/">Игрушки для ДЕВОЧЕК</a></div>
+                <div class="menu__item" data-id="168" data-childs="23"><a class="menu__head" href="//mamazin.com.ua/igrushki-dlja-malchikov/">Игрушки для МАЛЬЧИКОВ</a></div>
+                <div class="menu__item" data-id="224" data-childs="11"><a class="menu__head" href="//mamazin.com.ua/konstruktory/">КОНСТРУКТОРЫ</a></div>
+                <div class="menu__item" data-id="133" data-childs="18"><a class="menu__head" href="//mamazin.com.ua/igry-nastolnye/">Настольные игры</a></div>
+                <div class="menu__item" data-id="59" data-childs="19"><a class="menu__head" href="//mamazin.com.ua/igrushki/">Детские Игрушки</a></div>
+                <div class="menu__item" data-id="198" data-childs="17"><a class="menu__head" href="//mamazin.com.ua/igry-ulica-voda-pesok/">Товары для улицы, воды</a></div>
+                <div class="menu__item" data-id="1401" data-childs="21"><a class="menu__head" href="//mamazin.com.ua/zakolki/">Детские сумки, аксессуары</a></div>
+                <div class="menu__item" data-id="1137" data-childs="57"><a class="menu__head" href="//mamazin.com.ua/shkolnyjj-bazar/">Канцтовары, школьный базар</a></div>
+                <div class="menu__item" data-id="946" data-childs="20"><a class="menu__head" href="//mamazin.com.ua/igrushki-0-12-mes/">Игрушки, товары 0-12 мес</a></div>
+                <div class="menu__item" data-id="806" data-childs="13"><a class="menu__head" href="//mamazin.com.ua/den-rozhdene-rebenka/">Товары для праздника, подарки</a></div>
+                <div class="menu__item" data-id="929" data-childs="10"><a class="menu__head" href="//mamazin.com.ua/odezhda/">Постельное белье, текстиль</a></div>
+                <div class="menu__item" data-id="1251" data-childs="42"><a class="menu__head" href="//mamazin.com.ua/khoztovary-dlja-kukhni/">ХОЗТОВАРЫ</a></div>
+                <div class="menu__item" data-id="1797" data-childs="2">
+                  <a class="menu__head" href="//mamazin.com.ua/akcii-magazina/"><div class="sub_button"></div>Акции магазина</a>
+                  <div class="menu__body" id="drop1797">
+                    <div class="menu__back">
+                      &larr; назад
+                    </div>
+                    <ul class="menu__list menu__list_1">
+                      <li>
+                        <a class="menu__link" href="#" title="Акция 'Лови момент'">Акция "Лови момент"</a>
+                      </li>
+                      <li>
+                        <a class="menu__link" href="#" title="Акция 'Удачная покупка'">Акция "Удачная покупка"</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="header__bg"></div>
+            </div>
+          </div>
+          <div class="col-lg-6 sticky-md-none">
+            <div class="header-navigation-area d-none d-md-block">
+              <ul class="main-menu nav position-relative">
+                <li><a href="contact.html">Main page</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li><a href="contact.html">About</a></li>
+                <li><a href="contact.html">Blogs</a></li>
+                <li><a href="contact.html">Stock</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-8 col-sm-6 col-lg-2">
+            <div class="header-action-area">
+              <div class="header-action-search">
+                <button class="btn-search btn-search-menu">
+                  <i class="pe-7s-search"></i>
+                </button>
+              </div>
+              <div class="header-action-login">
+                <button class="btn-login" onclick="window.location.href='login-register.html'">
+                  <i class="pe-7s-users"></i>
+                </button>
+              </div>
+              <div class="header-action-wishlist">
+                <button class="btn-wishlist" onclick="window.location.href='shop-wishlist.html'">
+                  <i class="pe-7s-like"></i>
+                </button>
+              </div>
+              <div class="header-action-cart">
+                <button class="btn-cart cart-icon">
+                  <span class="cart-count">01</span>
+                  <i class="pe-7s-shopbag"></i>
+                </button>
+              </div>
+              <button class="btn-menu d-lg-none">
+                <i class="ion-navicon"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--== Start Side Menu ==-->
+    <aside class="off-canvas-wrapper">
+      <div class="off-canvas-inner">
+        <div class="off-canvas-overlay d-none"></div>
+        <!-- Start Off Canvas Content Wrapper -->
+        <div class="off-canvas-content">
+          <!-- Off Canvas Header -->
+          <div class="off-canvas-header">
+            <div class="close-action">
+              <button class="btn-close"><i class="pe-7s-close"></i></button>
+            </div>
+          </div>
+
+          <div class="off-canvas-item">
+            <!-- Start Mobile Menu Wrapper -->
+            <div class="res-mobile-menu">
+              <!-- Note Content Auto Generate By Jquery From Main Menu -->
+            </div>
+            <!-- End Mobile Menu Wrapper -->
+          </div>
+          <!-- Off Canvas Footer -->
+          <div class="off-canvas-footer"></div>
+        </div>
+        <!-- End Off Canvas Content Wrapper -->
+      </div>
+    </aside>
+    <!--== End Side Menu ==-->
+  </header>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'header',
+  data: () => ({
+  }),
+  computed: {
+    ...mapGetters([
+      'user',
+    ]),
+  },
+  created() {
+  },
+  methods: {
+    async logout () {
+      await this.$store.dispatch('deleteSession', { id: '', options: { fullResponse: true }})
+      this.$store.dispatch('clearUser')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrap-account {
+  padding-left: 20px;
+  margin-left: 19px;
+}
+</style>
