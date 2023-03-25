e44920cd7986bcf8f0c155e7d64b9c7a85a22552
ekzema <ekzemadev@gmail.com>
2023-03-25 21:19:18 +0200

    Added gem friendly_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_220722) do
-  create_table "brands", charset: "utf8", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2023_03_24_154325) do
+  create_table "brands", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_brands_on_name", unique: true
   end
 
-  create_table "categories", charset: "utf8", force: :cascade do |t|
+  create_table "categories", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
     t.integer "parent_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
     t.index ["name"], name: "index_categories_on_name", unique: true
     t.index ["parent_id"], name: "index_categories_on_parent_id"
+    t.index ["slug"], name: "index_categories_on_slug", unique: true
   end
 
-  create_table "product_categories", charset: "utf8", force: :cascade do |t|
+  create_table "product_categories", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "product_id"
     t.bigint "category_id"
     t.datetime "created_at", null: false
@@ -36,7 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_220722) do
     t.index ["product_id"], name: "index_product_categories_on_product_id"
   end
 
-  create_table "product_images", charset: "utf8", force: :cascade do |t|
+  create_table "product_images", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "product_id"
     t.string "image"
     t.datetime "created_at", null: false
@@ -44,7 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_220722) do
     t.index ["product_id"], name: "index_product_images_on_product_id"
   end
 
-  create_table "products", charset: "utf8", force: :cascade do |t|
+  create_table "products", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "status", default: 1
@@ -63,9 +65,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_220722) do
     t.integer "for_gender"
     t.integer "brand_id"
     t.integer "for_age"
+    t.string "slug"
+    t.index ["slug"], name: "index_products_on_slug", unique: true
   end
 
-  create_table "users", charset: "utf8", force: :cascade do |t|
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "patronymic"
