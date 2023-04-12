513935a43441f3e14c7e9f5d6f0cf4d4534c2de6
ekzema <ekzemadev@gmail.com>
2023-04-12 13:05:36 +0300

    Fixed migrate cart

--- db/migrate/20230410134710_create_carts.rb
+++ db/migrate/20230410134710_create_carts.rb
@@ -1,7 +1,8 @@
 class CreateCarts < ActiveRecord::Migration[7.0]
   def change
     create_table :carts do |t|
-      t.belongs_to :user, foreign_key: true
+      t.integer :user_id
+      t.index :user_id
       t.datetime :deleted_at
 
       t.timestamps
