13ad8c0b34f402540ab8f9c7ccdca7169ade1547
ekzema <ekzemadev@gmail.com>
2023-04-12 13:05:52 +0300

    Fixed migrate cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Cart < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :line_items, dependent: :destroy
 
   default_scope -> { where(deleted_at: nil) }
