ecb42c20fb777d9c98cefe67986eaaf2f7cceee2
ekzema <ekzemadev@gmail.com>
2023-05-22 12:07:33 +0300

    Rollback routes

--- app/serializers/cart_line_item_serializer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class CartLineItemSerializer < ActiveModel::Serializer
-  attributes :id, :quantity, :product
-
-  def product
-    ActiveModelSerializers::SerializableResource.new(object.product, serializer: ProductListSerializer)
-  end
-end
