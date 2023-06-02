cf0c93cc893a87748b2b1dc810a782bbe0a48032
ekzema <ekzemadev@gmail.com>
2023-06-02 13:45:24 +0300

    Changed user_serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,6 +3,15 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :phone, :first_name, :last_name, :patronymic, :wishlists_count, :line_items
 
+  def initialize(object, options = {})
+    options[:except] = [] unless options[:except]
+    super(object, options)
+  end
+
+  def attributes(*args)
+    super.except(*instance_options[:except])
+  end
+
   def line_items
     ActiveModelSerializers::SerializableResource.new(object.cart.line_items)
   end
