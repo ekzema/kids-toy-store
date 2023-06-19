22b8715ca2ac918b592279607379a7357cef5922
ekzema <ekzemadev@gmail.com>
2023-06-19 12:18:32 +0300

    Added create_service

--- app/services/v1/orders/create_service.rb
+++ app/services/v1/orders/create_service.rb
@@ -2,17 +2,16 @@
 
 class V1::Orders::CreateService < ApplicationService
   def call
-
+    current_user ? build_order(current_user.cart) : create_without_user
   end
 
   private
-
-  def create_with_user
-    # TODO create_with_user
-  end
 
   def create_without_user
     # TODO create_without_user
   end
 
+  def build_order(cart)
+    cart.build_order(params.except(:current_user))
+  end
 end
