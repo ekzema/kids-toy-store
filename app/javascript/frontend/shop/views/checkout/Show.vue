21411b4fc59b76273518a02f20e22e101988b493
ekzema <ekzemadev@gmail.com>
2023-06-23 12:30:05 +0300

    Updated checkout show

--- app/javascript/frontend/shop/views/checkout/Show.vue
+++ app/javascript/frontend/shop/views/checkout/Show.vue
@@ -45,134 +45,38 @@
               </div>
               <div class="col-12">
                 <div class="billing-select mb-20">
-                  <label>Country / Region <abbr class="required" title="required">*</abbr></label>
+                  <label>Payment method <abbr class="required" title="required">*</abbr></label>
                   <div class="select-style">
                     <select class="select-active">
-                      <option>Bangladesh</option>
-                      <option>Bahrain</option>
-                      <option>Azerbaijan</option>
-                      <option>Barbados</option>
-                      <option>Barbados</option>
+                      <option>Приват банк</option>
+                      <option>Наложеный платеж</option>
+                      <option>Другой способ оплаты</option>
                     </select>
                   </div>
-                </div>
-              </div>
-              <div class="col-12">
-                <div class="billing-info mb-20">
-                  <label>Street Address <abbr class="required" title="required">*</abbr></label>
-                  <input class="billing-address" placeholder="House number and street name" type="text">
-                  <input placeholder="Apartment, suite, unit, etc. (optional)" type="text">
-                </div>
-              </div>
-              <div class="col-12">
-                <div class="billing-info mb-20">
-                  <label>Town / City <abbr class="required" title="required">*</abbr></label>
-                  <input type="text">
                 </div>
               </div>
               <div class="col-12">
                 <div class="billing-select mb-20">
-                  <label>District <abbr class="required" title="required">*</abbr></label>
+                  <label>Delivery <abbr class="required" title="required">*</abbr></label>
                   <div class="select-style">
                     <select class="select-active">
-                      <option>Select an option…</option>
-                      <option>Barguna</option>
-                      <option>Bandarban</option>
-                      <option>Barbados</option>
-                      <option>Barbados</option>
+                      <option>Новая почта</option>
+                      <option>Укрпочта</option>
+                      <option>Самовывоз</option>
                     </select>
                   </div>
                 </div>
               </div>
               <div class="col-12">
                 <div class="billing-info mb-20">
-                  <label>Postcode / ZIP (optional) <abbr class="required" title="required">*</abbr></label>
-                  <input type="text">
+                  <label>Address <abbr class="required" title="required">*</abbr></label>
+                  <input class="billing-address" placeholder="House number and street name" type="text">
                 </div>
               </div>
-            </div>
-            <div class="checkout-account">
-              <input class="checkout-toggle" type="checkbox">
-              <span>Ship to a different address?</span>
-            </div>
-            <div class="different-address open-toggle mt-30">
-              <div class="row">
-                <div class="col-12">
-                  <div class="billing-info mb-20">
-                    <label>First name <abbr class="required" title="required">*</abbr></label>
-                    <input type="text">
-                  </div>
-                </div>
-                <div class="col-12">
-                  <div class="billing-info mb-20">
-                    <label>Last name <abbr class="required" title="required">*</abbr></label>
-                    <input type="text">
-                  </div>
-                </div>
-                <div class="col-12">
-                  <div class="billing-info mb-20">
-                    <label>Company name (optional) <abbr class="required" title="required">*</abbr></label>
-                    <input type="text">
-                  </div>
-                </div>
-                <div class="col-12">
-                  <div class="billing-select mb-20">
-                    <label>Country / Region <abbr class="required" title="required">*</abbr></label>
-                    <div class="select-style">
-                      <select class="select-active">
-                        <option>Bangladesh</option>
-                        <option>Bahrain</option>
-                        <option>Azerbaijan</option>
-                        <option>Barbados</option>
-                        <option>Barbados</option>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-12">
-                  <div class="billing-info mb-20">
-                    <label>Street Address <abbr class="required" title="required">*</abbr></label>
-                    <input class="billing-address" placeholder="House number and street name" type="text">
-                    <input placeholder="Apartment, suite, unit, etc. (optional)" type="text">
-                  </div>
-                </div>
-                <div class="col-12">
-                  <div class="billing-info mb-20">
-                    <label>Town / City <abbr class="required" title="required">*</abbr></label>
-                    <input type="text">
-                  </div>
-                </div>
-                <div class="col-12">
-                  <div class="billing-select mb-20">
-                    <label>District <abbr class="required" title="required">*</abbr></label>
-                    <div class="select-style">
-                      <select class="select-active">
-                        <option>Select an option…</option>
-                        <option>Barguna</option>
-                        <option>Bandarban</option>
-                        <option>Barbados</option>
-                        <option>Barbados</option>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-12">
-                  <div class="billing-info mb-20">
-                    <label>Postcode / ZIP (optional) <abbr class="required" title="required">*</abbr></label>
-                    <input type="text">
-                  </div>
-                </div>
-                <div class="col-12">
-                  <div class="billing-info mb-20">
-                    <label>Phone <abbr class="required" title="required">*</abbr></label>
-                    <input type="text">
-                  </div>
-                </div>
-                <div class="col-12">
-                  <div class="billing-info mb-20">
-                    <label>Email Address <abbr class="required" title="required">*</abbr></label>
-                    <input type="text">
-                  </div>
+              <div class="col-12">
+                <div class="billing-info mb-20">
+                  <label>Department number <abbr class="required" title="required">*</abbr></label>
+                  <input type="text">
                 </div>
               </div>
             </div>
@@ -196,57 +100,9 @@
                     <li>Enjoy The Rest T-Shirt × 1 <span>$39.59 </span></li>
                   </ul>
                 </div>
-                <div class="your-order-subtotal">
-                  <h3>Subtotal <span>$617.59</span></h3>
-                </div>
-                <div class="your-order-shipping">
-                  <span>Shipping</span>
-                  <ul>
-                    <li><input type="radio" name="shipping" value="info" checked="checked"><label>Free shipping</label></li>
-                    <li><input type="radio" name="shipping" value="info" checked="checked"><label>Flat rate: <span>$100.00</span></label></li>
-                    <li><input type="radio" name="shipping" value="info" checked="checked"><label>Local pickup: <span>$120.00</span></label></li>
-                  </ul>
-                </div>
                 <div class="your-order-total">
                   <h3>Total <span>$617.59 </span></h3>
                 </div>
-              </div>
-              <div class="payment-method">
-                <div class="pay-top sin-payment">
-                  <input id="payment_method_1" class="input-radio" type="radio" value="cheque" checked="checked" name="payment_method">
-                  <label for="payment_method_1"> Direct Bank Transfer </label>
-                  <div class="payment-box payment_method_bacs">
-                    <p>Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order will not be shipped until the funds have cleared in our account.</p>
-                  </div>
-                </div>
-                <div class="pay-top sin-payment">
-                  <input id="payment-method-2" class="input-radio" type="radio" value="cheque" name="payment_method">
-                  <label for="payment-method-2">Check payments</label>
-                  <div class="payment-box payment_method_bacs">
-                    <p>Please send a check to Store Name, Store Street, Store Town, Store State / County, Store Postcode.</p>
-                  </div>
-                </div>
-                <div class="pay-top sin-payment">
-                  <input id="payment-method-3" class="input-radio" type="radio" value="cheque" name="payment_method">
-                  <label for="payment-method-3">Cash on delivery </label>
-                  <div class="payment-box payment_method_bacs">
-                    <p>Pay with cash upon delivery. </p>
-                  </div>
-                </div>
-                <div class="pay-top sin-payment sin-payment-3">
-                  <input id="payment-method-4" class="input-radio" type="radio" value="cheque" name="payment_method">
-                  <label for="payment-method-4">PayPal <img alt="" src="assets/img/icons/payment-3.png"><a href="#">What is PayPal?</a></label>
-                  <div class="payment-box payment_method_bacs">
-                    <p>Pay via PayPal; you can pay with your credit card if you don’t have a PayPal account.</p>
-                  </div>
-                </div>
-              </div>
-              <div class="payment-condition">
-                <p>Your personal data will be used to process your order, support your experience throughout this website, and for other purposes described in our <a href="#">privacy policy</a>.</p>
-              </div>
-              <div class="payment-checkbox">
-                <input class="checkout-toggle" type="checkbox">
-                <span>Ship to a different address? <a href="#">terms and conditions *</a></span>
               </div>
             </div>
             <div class="place-order">
