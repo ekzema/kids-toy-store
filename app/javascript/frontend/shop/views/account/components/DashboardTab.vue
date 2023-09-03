1c7f0065a12c053edd783d1f60e04bd5c67a09da
ekzema <ekzemadev@gmail.com>
2023-09-03 22:51:09 +0300

    Added Added DashboardTab component

--- app/javascript/frontend/shop/views/account/components/DashboardTab.vue
+++ app/javascript/frontend/shop/views/account/components/DashboardTab.vue
@@ -1,5 +1,9 @@
 <template>
-  <h3>DashboardTab!</h3>
+  <h3>Dashboard</h3>
+  <div class="welcome">
+    <p>Hello, <strong>Alex Tuntuni</strong> (If Not <strong>Tuntuni !</strong><a href="login-register.html" class="logout"> Logout</a>)</p>
+  </div>
+  <p class="mb-0">From your account dashboard. you can easily check & view your recent orders, manage your shipping and billing addresses and edit your password and account details.</p>
 </template>
 
 <script>
