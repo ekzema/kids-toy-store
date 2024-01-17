caf80d1a5e9173c1006fb0d4dd29114abb3a40f3
ekzema <ekzemadev@gmail.com>
2024-01-17 08:48:09 +0200

    Added pagination component

---
+++ app/javascript/frontend/shop/components/Pagination.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="row mb-md-50">
+    <div class="col-lg-12">
+      <div class="pagination-area">
+        <nav>
+          <ul class="page-numbers">
+            <li>
+              <a class="page-number active" href="blog.html">1</a>
+            </li>
+            <li>
+              <a class="page-number" href="blog.html">2</a>
+            </li>
+            <li>
+              <a class="page-number" href="blog.html">3</a>
+            </li>
+            <li>
+              <a class="page-number next" href="blog.html">
+                <i class="fa fa-angle-right"></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ShopPagination',
+  data() {
+    return {
+
+    }
+  },
+  created () {
+  },
+  methods: {
+
+  }
+}
+</script>
+
