4c4180bd45e13f1e6b45cb542e05ff3d1627a160
ekzema <ekzemadev@gmail.com>
2024-01-29 15:13:21 +0200

    Fixed cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_to, "/home/deploy/sites/kids-toy-store"
 append :linked_files, '.env'
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage", "public/packs"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage", "public/packs", "node_modules"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -39,6 +39,7 @@ set :keep_releases, 5
 set :rbenv_ruby, '3.1.2'
 set :nvm_node, 'v16.20.2'
 set :nvm_type, :user
-set :nvm_map_bins, %w{node npm yarn}
+set :nvm_map_bins, %w{node npm yarn rake}
+
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
