5d3b38b0a2d731599637d9f3233d186f891b1329
ekzema <ekzemadev@gmail.com>
2024-02-05 11:51:31 +0200

    Change .env file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,14 +2,14 @@ require 'dotenv/load'
 
 set :application, ENV['APP_NAME']
 set :rails_env,   'production'
-set :repo_url,    'git@github.com:ekzema/kids-toy-store.git'
-set :branch,      'capistrano'
-set :user,        'deploy'
+set :repo_url,    ENV['DEPLOY_REPO']
+# set :branch,      'capistrano'
+set :user,        ENV['DEPLOY_USER']
 set :deploy_to,   "/home/#{fetch(:user)}/sites/#{fetch(:application)}"
 
-server "172.17.0.1",
+server ENV['DEPLOY_SERVER'],
        user: fetch(:user),
-       port: "2222",
+       # port: "2222",
        roles: %w{web app db},
        primary: true,
        ssh_options: {
