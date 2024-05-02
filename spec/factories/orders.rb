43b7891a988a807dc1462d13b9a5720c0b6f1053
ekzema <ekzemadev@gmail.com>
2024-05-02 16:50:58 +0300

    Added rspec for order model

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,16 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :order do
-    first_name { "MyString" }
+    cart
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    patronymic { Faker::Name.male_first_name }
+    email { Faker::Internet.email }
+    phone { '+380956036734' }
+    pay_type { :privat_bank }
+    delivery { :novaya_pochta }
+    city { Faker::Address.city }
+    department_number { 20 }
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,54 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Order do
+  let(:order) { create(:order) }
+
+  describe 'associations' do
+    it { should belong_to(:cart) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:patronymic) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:phone) }
+    it { should validate_presence_of(:pay_type) }
+    it { should validate_presence_of(:delivery) }
+    it { should validate_length_of(:first_name).is_at_least(3) }
+    it { should validate_length_of(:last_name).is_at_least(3) }
+    it { should validate_length_of(:patronymic).is_at_least(3) }
+    it { should_not allow_value('invalid_email').for(:email) }
+    it { should allow_value('valid@example.com').for(:email) }
+
+    context 'when delivery method return false' do
+      before do
+        allow(order).to receive(:delivery_method?).and_return(false)
+      end
+
+      it 'validates presence of city' do
+        expect(order).not_to validate_presence_of(:city)
+      end
+
+      it 'validates presence of department_number' do
+        expect(order).not_to validate_presence_of(:department_number)
+      end
+    end
+
+    context 'when delivery method return true' do
+      before do
+        allow(order).to receive(:delivery_method?).and_return(true)
+      end
+
+      it 'validates presence of city' do
+        expect(order).to validate_presence_of(:city)
+      end
+
+      it 'validates presence of department_number' do
+        expect(order).to validate_presence_of(:department_number)
+      end
+    end
+  end
 end
